fa79e948ab8b951571b477abc164ea3b2448cc8f
joshturn <talktojosh@gmail.com>
2015-06-03 15:50:21 -0700

    Added ability for server to respond to GET requests, passes first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === "GET" && req.url === "/") {
+    fs.readFile('/Users/student/2015-05-web-historian/web/public/index.html', function(err, data){
+      if (err) {
+        helpers.sendResponse(res, null, err);
+      } else {
+        helpers.sendResponse(res, data, 200);
+      }
+    });
+  }
+  // res.end(archive.paths.list);
 };
